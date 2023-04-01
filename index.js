94163f7c13995cda2f88f2e240168beab90b2d13
antpb <itartist.pdf@gmail.com>
2023-04-01 16:49:17 -0500

    adjusts for gpt4

--- index.js
+++ index.js
@@ -29,76 +29,122 @@
  */
 
 async function handleRequest(request) {
-    if (request.method === 'OPTIONS') {
-        return handleOptions(request);
-    } else {
-        return handleFetch(request);
-    }
-}
+	if (request.method === "OPTIONS") {
+	  return handleOptions(request);
+	} else {
+	  return handleFetch(request);
+	}
+  }
+  
+  async function handleFetch(request) {
+	// Extract the bearer token from the request headers
+	const authorization = request.headers.get("Authorization");
+	if (!authorization) {
+	  return new Response("Unauthorized", { status: 401 });
+	}
+  
+  const data = await request.json();
+  console.log("data", data);
+  const history = data.Input.messages ?? [];
+  console.log("history", history);
+  const lastUserMessage = history.length > 1 ? history[history.length - 2].split(": ")[1] : null;
+  const lastAssistantMessage = history.length > 0 ? history[history.length - 1].split(": ")[1] : null;
+  
+	//let prompt = data.inputs.personality;
+	let prompt = data.Input.personality;
+	let finalPrompt = prompt
+	  .replaceAll("#speaker", data.Input.Speaker)
+	  .replaceAll("#input", data.Input.Input)
+	  .replaceAll("#agent", data.Input.Agent)
+	  .replaceAll("#conversation", data.Input.Conversation)
+	  .replaceAll("undefined\n", "")
+	  .replaceAll("undefined", "")
+	  .slice(-5000);
+  console.log("finalPrompt", finalPrompt);
+	const token = authorization.split(" ")[1];
+	console.log(data.Input);
+  const messages = [
+	{
+	  "role": "system",
+	  "content": data.Input.personality,
+	},
+	{
+	  "content": "Hello, how are you?",
+	  "role": "user",
+	},
+	{
+	  "content": "I'm doing well! Excited to be with you here in the metaverse!",
+	  "role": "assistant",
+	},
+  ];
+  
+  if (lastUserMessage) {
+	messages.push({
+	  "content": lastUserMessage,
+	  "role": "user",
+	});
+  }
+  
+  if (lastAssistantMessage) {
+	messages.push({
+	  "content": lastAssistantMessage,
+	  "role": "assistant",
+	});
+  }
+  
+  messages.push({
+	"content": data.Input.Input,
+	"role": "user",
+  });
+  
+  const postData = {
+	model: "gpt-4",
+	messages,
+	temperature: 0.7,
+	max_tokens: 500,
+	stop: "###",
+  };
 
-async function handleFetch(request) {
-    // Extract the bearer token from the request headers
-    const authorization = request.headers.get('Authorization');
-    if (!authorization) {
-        return new Response('Unauthorized', { status: 401 });
-    }
-
-    const data = await request.json();
-    let prompt = data.inputs.personality;
-    let prompt = data.Input.personality;
-    let finalPrompt = prompt
-        .replaceAll('#speaker', data.Input.Speaker)
-        .replaceAll('#input', data.Input.Input)
-        .replaceAll('#agent', data.Input.Agent)
-        .replaceAll('#conversation', data.Input.Conversation)
-        .replaceAll('undefined\n','' ).replaceAll('undefined','')
-        .slice(-5000)
-
-    const token = authorization.split(' ')[1];
-    const postData = {
-        prompt: finalPrompt	,
-        max_tokens: 500,
-        stop : ["###"],
-        temperature: 0.7,
-    };
-
-    // Make the first request to the Davinci model
-    const davinciResponse = await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        },
-        body: JSON.stringify(postData)
-    });
-
-    // Process the response from the Davinci model
-    const davinciData = await davinciResponse.json();
-    console.log(davinciData);
-
-    // Return the results to the client
-    const headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
-    };
-    return new Response(JSON.stringify({ davinciData }), {
-        headers
-    });
-}
-
-function handleOptions(request) {
-    // Make sure the necessary headers are present
-    // for this to be a valid pre-flight request
-    const headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    };
-    return new Response(null, { headers });
-}
-
-addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
-});
+  // Make the request to the OpenAI API
+  const gptResponse = await fetch("https://api.openai.com/v1/chat/completions", {
+	method: "POST",
+	headers: {
+	  "Content-Type": "application/json",
+	  Authorization: `Bearer ${token}`,
+	},
+	body: JSON.stringify(postData),
+  });
+  
+  // Process the response from the OpenAI API
+  const gptData = await gptResponse.json();
+  if (gptData.choices && gptData.choices.length > 0) {
+	console.log(gptData.choices[0].text);
+  } else {
+	console.log("No completion found.", gptData);
+  }
+  
+  // Return the results to the client
+  const headers = {
+	"Access-Control-Allow-Origin": "*",
+	"Content-Type": "application/json",
+	"Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+	"Access-Control-Allow-Headers": "Content-Type, Authorization",
+  };
+  return new Response(JSON.stringify(gptData), { headers });
+  }
+  
+  function handleOptions(request) {
+	// Make sure the necessary headers are present
+	// for this to be a valid pre-flight request
+	const headers = {
+	  "Access-Control-Allow-Origin": "*",
+	  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+	  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+	};
+	return new Response(null, { headers });
+  }
+  
+  addEventListener("fetch", (event) => {
+	event.respondWith(handleRequest(event.request));
+  });
+  
